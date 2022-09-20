b522c4c9ca75c56eb8752d9e0ce49964f97121c3
AlexanderKudretov <113973614+AlexanderKudretov@users.noreply.github.com>
2022-09-20 10:51:28 +0300

    Add files via upload

---
+++ DatailViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+	[QueryProperty("Text", "Text")]
+	public partial class DatailViewModel : ObservableObject
+	{
+		[ObservableProperty]
+		string text;
+
+		[RelayCommad]
+
+		async Task Goback()
+		{
+			await Shell.Current.GoToAsync("..");
+		}
+	}
---
+++ MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		public partial class MainViewModel : Outputter
+	Tasks = new ObservableCollection<string>();
+		this.connectivity = connectivity; 
+		}
+[ObservableProperty]
+ObservableCollection<string> tasks;
+
+[ObservableProperty]
+string taskTitle;
+
+[RelayCommand]
+
+async Task Add()
+{
+	if (string.IsNullOrWhiteSpace(taskTitle))
+		return;
+	if (connectivity.NetworkAccess != NetworkAccess.Internet)
+	{
+		await Shell.Current.DisplayAlert("ПР №1", "нет соединеия с интернетом", "ОК")
+return;
+	}
+	Tasks.Add(TaskTitle);
+
+	TaskTitle = string.Empty
+		[RelayCommand]
+		Void delete(string s)
+	{
+		if (Tasks.Contains(s))
+		{
+			Tasks.Remove(s);
+		}
+	}
+	[RelayCommand]
+	async Task Tap(string s)
+	{
+		await Shell.Current.GoToAsync($"{nameof(DetailPage)}?Text={s}");
+
+
+
+
+
